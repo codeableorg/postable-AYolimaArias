6b101f804c9126e3981f48c19fc46b6ddcb3dbf6
Angelica <angelica.arias33@gmail.com>
2024-04-25 00:41:08 -0500

    [ADD] Tables created and migration successful.

--- src/db/migrations/2024.04.25T04.27.41.create-users.ts
+++ src/db/migrations/2024.04.25T04.27.41.create-users.ts
@@ -1,8 +1,19 @@
 import { Migration } from "../scripts/dbMigrate";
 
 export const up: Migration = async (params) => {
-  params.context.query(`RAISE EXCEPTION 'up migration not implemented'`);
+  return params.context.query(`CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE,
+    firstName VARCHAR(255),
+    lastName VARCHAR(255),
+    role VARCHAR(20) NOT NULL DEFAULT 'user' CHECK (role IN ('admin', 'user')),
+    createdAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updatedAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  );
+  `);
 };
 export const down: Migration = async (params) => {
-  params.context.query(`RAISE EXCEPTION 'down migration not implemented'`);
+  return params.context.query(`DROP TABLE users;`);
 };
