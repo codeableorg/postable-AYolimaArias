9db2d9a2f46a91215be859bed6f4305a176e438b
Angelica <angelica.arias33@gmail.com>
2024-04-30 00:05:05 -0500

    [ADD] TEST POSTS completed in test database.

--- src/tests/posts.test.ts
+++ src/tests/posts.test.ts
@@ -1,29 +1,55 @@
 import { describe, beforeEach, it, expect } from "vitest";
 import request from "supertest";
-import { app } from "../app"; // Import your Express app
-
+import { truncateTable } from "../db/utils";
+import { app } from "../app";
 import * as db from "../db";
+const jwt = require("jsonwebtoken");
+const jwtSecret = "ultra-secret";
+
+const testUsers = [
+  {
+    id: 1,
+    username: "usuario 1",
+    password: 123456,
+    email: "prueba@mail.com",
+    firstname: "Testino",
+    lastname: "diprueba",
+    role: "user",
+  },
+  {
+    id: 2,
+    username: "usuario 2",
+    password: 123456,
+    email: "testino@mail.com",
+    firstname: "Diprueba",
+    lastname: "Test",
+    role: "user",
+  },
+];
 
 const testPosts = [
-  { userid: 4, content: "Im a testing" },
+  { userid: 1, content: "Im a testing" },
   { userid: 2, content: "Hello world" },
 ];
 
-// const updates = [
-//   {
-//     content: "I'm a new  testing",
-//     userid: "2024-04-28T12:00:00.000Z",
-//     updatedat: "2024-04-28T12:30:00.000Z",
-//   },
-//   {
-//     content: "I'm a update",
-//     createdat: "2024-04-29T12:00:00.000Z",
-//     updatedat: "2024-04-29T12:30:00.000Z",
-//   },
-// ];
+//USERS TABLE:
+beforeEach(async () => {
+  await truncateTable("users");
+  const values = testUsers
+    .map(
+      (user) =>
+        `('${user.username}','${user.firstname}','${user.lastname}','${user.email}','${user.password}','${user.role}')`
+    )
+    .join(", ");
+  let query = `INSERT INTO users (username, firstname, lastname,email, password, role) VALUES ${values} RETURNING *`;
 
-describe("Posts API", () => {
+  await db.query(query);
+});
+
+//POSTS TABLE:
+describe("posts API", () => {
   beforeEach(async () => {
+    await truncateTable("posts");
     const values = testPosts
       .map((post) => `('${post.userid}','${post.content}')`)
       .join(", ");
@@ -31,10 +57,76 @@ describe("Posts API", () => {
 
     await db.query(query);
   });
+
+  // GET/posts:
   it("should get all posts", async () => {
     const response = await request(app).get("/posts");
     expect(response.statusCode).toBe(200);
     expect(response.body.ok).toBeTruthy();
-    // expect(response.body.data).toHaveLength(2);
+    expect(response.body.data).toHaveLength(1);
+  });
+
+  //GET/posts/:usename:
+  it("should get the user posts with username", async () => {
+    const response = await request(app).get("/posts/usuario 1");
+    expect(response.statusCode).toBe(200);
+    expect(response.body.ok).toBeTruthy();
+  });
+
+  //POST/posts:
+  it("should create a post", async () => {
+    //TOKEN
+    const payload = { userId: 1, userRole: "user" };
+    const token = jwt.sign(payload, jwtSecret, { expiresIn: "120m" });
+
+    const postData = { content: "holis" };
+    let response = await request(app)
+      .post("/posts")
+      .set("Authorization", `Bearer ${token}`) // Adjunto el token a la solicitud
+      .send(postData);
+    expect(response.statusCode).toBe(201);
+    expect(response.body.data).toMatchObject(postData);
+    const newId = response.body.data.username;
+    response = await request(app).get(`/posts/${newId}`);
+    expect(response.statusCode).toBe(200);
+  });
+
+  //PATCH/posts/:id:
+  it("should update a post", async () => {
+    //TOKEN:
+    const payload = { userId: 1, userRole: "user" };
+    const token = jwt.sign(payload, jwtSecret, { expiresIn: "120m" });
+
+    const updates = { content: "im a update" };
+    const response = await request(app)
+      .patch("/posts/2")
+      .set("Authorization", `Bearer ${token}`)
+      .send(updates);
+    expect(response.statusCode).toBe(200);
+    expect(response.body.data).toMatchObject(updates);
+  });
+
+  //POST/:postId/like
+  it("should give like to a post", async () => {
+    //TOKEN:
+    const payload = { userId: 1, userRole: "user" };
+    const token = jwt.sign(payload, jwtSecret, { expiresIn: "120m" });
+
+    const response = await request(app)
+      .post("/posts/1/like")
+      .set("Authorization", `Bearer ${token}`);
+    expect(response.statusCode).toBe(200);
+  });
+
+  //DELETE/:postId/like:
+  it("should delete the like post", async () => {
+    //TOKEN:
+    const payload = { userId: 1, userRole: "user" };
+    const token = jwt.sign(payload, jwtSecret, { expiresIn: "120m" });
+
+    const response = await request(app)
+      .delete("/posts/1/like")
+      .set("Authorization", `Bearer ${token}`);
+    expect(response.statusCode).toBe(200);
   });
 });
